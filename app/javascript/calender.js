7c0cf05ed8fc06db7ced9ef62e00f8d97b3866df
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-13 20:17:07 +0900

    カレンダー詳細設定

--- app/javascript/calender.js
+++ app/javascript/calender.js
@@ -6,8 +6,17 @@ document.addEventListener('turbolinks:load', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, interactionPlugin ]
+    plugins: [ dayGridPlugin, interactionPlugin ],
+    locale: 'ja',
+    timeZone: 'Asia/Tokyo',
+    firstDay: 1,
+    headerToolbar: {
+      start: '',
+      center: 'title',
+      end: 'today prev,next' 
+    },
   });
 
+  
   calendar.render();
 });
