b05a1e26e4514cf2903d9516e683d3f10a3b68a2
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-20 23:24:47 +0900

    fullcalendar修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except:[:index, :show]
 
   def index
     @events = Event.all
--- app/javascript/packs/calender.js
+++ app/javascript/packs/calender.js
@@ -3,69 +3,73 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { isInt } from 'fullcalendar';
 
-document.addEventListener('turbolinks:load', function() {
-  var calendarEl = document.getElementById('calendar');
+const calendar = () => {
+  document.addEventListener('turbolinks:load', function() {
+    var calendarEl = document.getElementById('calendar');
 
-  const calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, interactionPlugin],
-    locale: 'ja',
-    timeZone: 'Asia/Tokyo',
-    firstDay: 1,
-    customButtons: {
-      myCustomButton: {
-        text: 'みんなの投稿',
-        click: function(){
-          location.href = "/index";
+    const calendar = new Calendar(calendarEl, {
+      plugins: [ dayGridPlugin, interactionPlugin],
+      locale: 'ja',
+      timeZone: 'Asia/Tokyo',
+      firstDay: 1,
+      customButtons: {
+        myCustomButton: {
+          text: 'みんなの投稿',
+          click: function(){
+            location.href = "/index";
+          }
         }
-      }
-    },
-    headerToolbar: {
-      start: 'myCustomButton',
-      center: 'title',
-      end: 'today prev,next' 
-    },
-    buttonText: {
-      today: '今日',
-      month: '月',
-    },
+      },
+      headerToolbar: {
+        start: 'myCustomButton',
+        center: 'title',
+        end: 'today prev,next' 
+      },
+      buttonText: {
+        today: '今日',
+        month: '月',
+      },
 
-    events: '/events.json',
+      events: '/events.json',
+      
+
+      expandRows: true,
+      height: "auto",
+      dayCellContent: function(e) {
+        e.dayNumberText = e.dayNumberText.replace('日', '');
+      },
+      dateClick: function(e){ 
+        location.href = `/events/new?date=${e.dateStr}` ;
+      },
+      eventClick: function(e){
+        location.href = `/events/${e.event.id}`
+      },
+
+      eventContent: function(e) {
+        let arrayOfDomNodes = []
+        let titleEvent = document.createElement('div')
+        if(e.event._def.title) {
+          titleEvent.innerHTML = e.event._def.title
+          titleEvent.classList = "fc-event-title fc-sticky"
+        }
+
+        let imgEventWrap = document.createElement('div')
+        if(e.event.extendedProps.image_url) {
+          let imgEvent = '<img src="'+e.event.extendedProps.image_url+'" >'
+          imgEventWrap.classList = "fc-event-img"
+          imgEventWrap.innerHTML = imgEvent;
+        }
+
+        arrayOfDomNodes = [ titleEvent,imgEventWrap ]
+
+        return { domNodes: arrayOfDomNodes }
+      },
+    });
+
+
     
-
-    expandRows: true,
-    height: "auto",
-    dayCellContent: function(e) {
-      e.dayNumberText = e.dayNumberText.replace('日', '');
-    },
-    dateClick: function(e){ 
-      location.href = `/events/new?date=${e.dateStr}` ;
-    },
-    eventClick: function(e){
-      location.href = `/events/${e.event.id}`
-    },
-
-    eventContent: function(e) {
-      let arrayOfDomNodes = []
-      let titleEvent = document.createElement('div')
-      if(e.event._def.title) {
-        titleEvent.innerHTML = e.event._def.title
-        titleEvent.classList = "fc-event-title fc-sticky"
-      }
-
-      let imgEventWrap = document.createElement('div')
-      if(e.event.extendedProps.image_url) {
-        let imgEvent = '<img src="'+e.event.extendedProps.image_url+'" >'
-        imgEventWrap.classList = "fc-event-img"
-        imgEventWrap.innerHTML = imgEvent;
-      }
-
-      arrayOfDomNodes = [ titleEvent,imgEventWrap ]
-
-      return { domNodes: arrayOfDomNodes }
-    },
+    calendar.render();
   });
+};
 
-
-  
-  calendar.render();
-});
+window.addEventListener('load', calendar);
